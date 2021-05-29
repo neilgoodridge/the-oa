ba11699c6ad9a1ccbd20418e3448a04ccc806e2e
Annie Walsh <annie@anyonething.co.uk>
2021-05-29 16:15:44 +0100

    user dashboard route, controller, view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :create, :new ]
 
-def create
-end
+  def create
+  end
 
-def new
-end
+  def new
+  end
 
-def show
-end
+  def show
+  end
 
-end
+  def dashboard
+  end
+
+  end
