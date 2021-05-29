fd7d6543cd51443b24b0cc0665f78d2bf37ee856
Arthur Maroquene Froissart <arthurmf.amf@gmail.com>
2021-05-29 16:30:01 +0100

    Task controller, view annd routes DONE

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
     resources :causes, only: [ :index, :show ] do
+      resources :tasks, only:  [ :index, :create ] do
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only:  [:show, :create, :new ] do
