fa75882ba8334d66165f2330b0fdebeb87d387d9
Annie Walsh <annie@anyonething.co.uk>
2021-05-29 11:39:57 +0100

    seed file start

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seed: Deleting existing records...'
+
+Task.destroy_all
+Action.destroy_all
+Cause.destroy_all
+User.destroy_all
+
+puts 'Seed: Seeding...'
+
+
+user1 = User.create!(
+  email: "malcolm@gmail.com",
+  name: "Malcolm",
+  password: "123456",
+  password_confirmation: "123456"
+  points: 0
+)
+
+
+cause1 = Cause.create!(
+  name: ' #blacklivesmatter'
+)
+
+cause2 = Cause.create!(
+  name: '#equalpay'
+)
+
+cause3 = Cause.create!(
+  name: '#seaspiracy'
+)
+
+cause4 = Cause.create!(
+  name: '#StopKnifeCrime'
+)
+
+cause5 = Cause.create!(
+  name: '#stopasianhate'
+)
+
+cause6 = Cause.create!(
+  name: '#KillTheBill!'
+)
+
+cause7 = Cause.create!(
+  name: '#climatechange'
+)
+
+cause8 = Cause.create!(
+  name: '#fairpay'
+)
+
+cause9 = Cause.create!(
+  name: '#'
+)
+
+task1 = Task.create!(
+  name:
+  points:
+  content:
+  time:
+  )
+
+action1 = Action.create!(
+  completed:
+  user_id:
+  )
+
+puts 'Seed: Finished seeding!'
