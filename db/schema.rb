defdaa323c08d554a3eef2de09668a351d0bc60e
Annie Walsh <annie@anyonething.co.uk>
2021-05-29 12:13:15 +0100

    add task_id to actions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_202806) do
+ActiveRecord::Schema.define(version: 2021_05_29_111200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_05_27_202806) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "task_id", null: false
+    t.index ["task_id"], name: "index_actions_on_task_id"
     t.index ["user_id"], name: "index_actions_on_user_id"
   end
 
@@ -54,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_202806) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "actions", "tasks"
   add_foreign_key "actions", "users"
   add_foreign_key "tasks", "causes"
 end
