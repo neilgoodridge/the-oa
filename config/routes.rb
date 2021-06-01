ae68bb769cdde2c9a8a588186a1fc7487bdea61c
Arthur Maroquene Froissart <arthurmf.amf@gmail.com>
2021-06-01 21:32:53 +0100

    created the link between the causes & task controller in cause_task_show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+
     resources :users do
       collection do
         get "dashboard"
       end
     end
-
+    # get 'cause_task_show', to: 'causes#cause_task_show'
+    get '/causes/:id/tasks', to: 'causes#cause_task_show', as: 'cause_task'
     resources :causes, only: [ :index, :show ] do
       resources :tasks, only:  [ :index, :create ] do
     end
@@ -15,3 +18,5 @@ Rails.application.routes.draw do
   resources :users, only:  [:show, :create, :new ] do
   end
 end
+
+
