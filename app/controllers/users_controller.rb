2ebe0bf22d92c7636f07dcb6537f680e215f7ddb
Arthur Maroquene Froissart <arthurmf.amf@gmail.com>
2021-06-05 14:25:20 +0100

    dashboard view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :create, :new ]
 
   def dashboard
+    @cause_selected = current_user.tasks
   end
 end
