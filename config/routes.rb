b63db48d4c3bdd7c87e2af264e1e1521ee36d558
Annie Walsh <annie@anyonething.co.uk>
2021-06-05 12:26:58 +0100

    overrides devise, adds store_task method with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'pages#home'
 
 
     resources :users do
       collection do
         get "dashboard"
+        post "store_tasks"
       end
     end
     # get 'cause_task_show', to: 'causes#cause_task_show'
