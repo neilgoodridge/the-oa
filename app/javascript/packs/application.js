6504c3f531d81136484bd889795fcb7a413e43ad
Neil Goodridge <>
2021-06-12 11:59:27 +0100

    updated carousel without impacting cause page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("causes")
 require("home")
+require("carousel")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
