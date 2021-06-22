7decc7c59e0cc3132774483260de7e377598e82e
Neil Goodridge <>
2021-06-22 21:42:42 +0100

    fixed JS for arrows on cause index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,6 +36,9 @@ import { loadDynamicBannerText } from '../components/banner';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  loadDynamicBannerText();
+  const bottomHalf = document.querySelector(".bottom-half")
+  if (bottomHalf) {
+    loadDynamicBannerText();
+  }
 });
 
